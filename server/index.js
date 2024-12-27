fb4853ae14bfc023d4aae0a34989c8c505366ebe
Chetan Anand <chetan108@outlook.com>
2024-12-27 19:58:59 +0530

    fix port

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
 import {
     getServerInfo,
     getServerSettings,
 } from "./controllers/serverController.js";
-dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 
 app.use(express.json());
 
